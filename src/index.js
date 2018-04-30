58677e5cff577bce15218c89fcd5bc33ef432ecb
Jarek Figura <jarek.figura@icloud.com>
2018-04-30 15:59:27 +0200

    Display X when clicked on button in Square component.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 
 class Square extends React.Component {
   state = {
-    value: null,
+    value: null
   };
 
   render() {
     return (
-        <button className="square" onClick={() => alert('click')}>
-          {this.props.value}
+        <button className="square" onClick={() => this.setState({value: 'X'})}>
+          {this.state.value}
         </button>
     );
   }
