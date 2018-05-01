61d6df43c9357f8ef0f924f0b4361f71d8b6a909
Jarek Figura <jarek.figura@icloud.com>
2018-05-01 13:10:06 +0200

    Building the history of moves: 1. Adding initial state to Game component.

--- src/index.js
+++ src/index.js
@@ -69,6 +69,29 @@ class Board extends React.Component {
   }
 }
 
+class Game extends React.Component {
+  state = {
+    history: [{
+      squares: Array(9).fill(null),
+    }],
+    xIsNext: true,
+  };
+
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -87,22 +110,6 @@ function calculateWinner(squares) {
     }
   }
   return null;
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
 }
 
 // ========================================
