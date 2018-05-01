d1688af8c51da0a03cb1ad78564d47b7959d5750
Jarek Figura <jarek.figura@icloud.com>
2018-05-01 12:08:56 +0200

    Passing a onClick prop from Board to Square.

--- src/index.js
+++ src/index.js
@@ -22,7 +22,12 @@ class Board extends React.Component {
   };
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+        <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
+    );
   }
 
   render() {
